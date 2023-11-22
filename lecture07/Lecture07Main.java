1a8533ce9c3831ab7f7f45a12725be81f6cb3b90
Lee Youngjin <youngjin8743@gmail.com>
2023-11-23 07:36:22 +0900

    lecture 07

---
+++ lecture07/Lecture07Main.java
@@ -0,0 +1,29 @@
+package lecture07;
+
+import java.io.IOException;
+import org.jetbrains.annotations.NotNull;
+
+public class Lecture07Main {
+
+    public static void main(String[] args) throws IOException {
+
+        JavaFilePrinter printer = new JavaFilePrinter();
+        printer.readFile();
+    }
+
+    private int parseIntOrThrow(@NotNull String str) {
+        try {
+            return Integer.parseInt(str);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(String.format("주어진 %s는 숫자가 아닙니다", str));
+        }
+    }
+
+    private Integer parseIntOrThrow2(@NotNull String str) {
+        try {
+            return Integer.parseInt(str);
+        } catch (NumberFormatException e) {
+            return null;
+        }
+    }
+}
